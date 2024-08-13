e5c262feeef52ea6b378f2dce3b21177d940e233
Juan Carvajal <pcjvan@gmail.com>
2024-08-13 16:20:37 -0500

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-      <div className="wrapper">
-        <h1>Welcome App</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <h1 className="visually-hidden">brainblox</h1>
+      <main className="wrapper">
+        <Routes>
+          <Route path="*" element={<h1> 404 - Not found page</h1>} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
