7ee8f92be96f4c243ffb34f7e635a6ec2514faf7
Juan Carvajal <pcjvan@gmail.com>
2024-08-14 19:58:27 -0500

    add notes menu

---
+++ src/components/NotesMenu/index.tsx
@@ -0,0 +1,11 @@
+import './NotesMenu.scss';
+export default function NotesMenu() {
+
+  return (
+    <section className="menu">
+      <h2 className="visually-hidden">Notes Menu</h2>
+      <div className="menu__wrapper">
+      </div>
+    </section>
+  );
+}
