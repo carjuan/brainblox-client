6f47aed422486a04953b7a855c2d094058adfe73
Juan Carvajal <pcjvan@gmail.com>
2024-08-14 20:00:50 -0500

    add notes palette toggler

--- src/components/NotesMenu/index.tsx
+++ src/components/NotesMenu/index.tsx
@@ -1,10 +1,21 @@
 import './NotesMenu.scss';
+import NotesPaletteToggler from '../NotesPaletteToggler';
+import { useState } from 'react';
 export default function NotesMenu() {
+  const [shouldAnimate, setShouldAnimate] = useState<boolean>(false);
+
+  const togglePaletteMenu = () => {
+    setShouldAnimate((shouldAnimate: boolean): boolean => !shouldAnimate);
+  };
 
   return (
     <section className="menu">
       <h2 className="visually-hidden">Notes Menu</h2>
       <div className="menu__wrapper">
+        <NotesPaletteToggler
+          shouldAnimate={shouldAnimate}
+          togglePalette={togglePaletteMenu}
+        />
       </div>
     </section>
   );
