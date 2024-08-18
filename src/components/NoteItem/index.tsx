eec8314b57ac5bdf5840644abf1dd5220aaa232b
Juan Carvajal <pcjvan@gmail.com>
2024-08-17 20:17:01 -0500

    use notesService to make a note draggable

--- src/components/NoteItem/index.tsx
+++ src/components/NoteItem/index.tsx
@@ -1,22 +1,27 @@
 import 'quill/dist/quill.snow.css';
 import './NoteItem.scss';
-import noteManager from '../../NotesManager';
+import notesServices from '../../services/notes';
 import { Note as NoteProps } from '../../NotesProvider';
 import { useEffect, useRef } from 'react';
 
 export default function NoteItem({ noteFactory, color }: NoteProps) {
+  const draggableRef = useRef<HTMLDivElement | null>(null);
   const noteRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (noteRef.current) {
-      const note = new noteFactory(noteRef.current, {
+      new noteFactory(noteRef.current, {
         theme: 'snow',
       });
+    }
+
+    if (draggableRef.current) {
+      notesServices.makeDraggable(draggableRef.current);
     }
   }, []);
 
   return (
-    <div className={`note--${color}`}>
+    <div ref={draggableRef} className={`note--${color}`}>
       <div className="note__container" ref={noteRef}></div>
     </div>
   );
