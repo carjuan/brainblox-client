5f920d916cbe5ee6a277abaa49300dbdd76a8eb5
Juan Carvajal <pcjvan@gmail.com>
2024-08-20 19:55:17 -0500

    add styles for adding a new workspace

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -1,6 +1,7 @@
 import './Select.scss';
 import { useState, useRef } from 'react';
 import Option from '../Option';
+import AddItem from '../../AddItem';
 import type WorkspacesSelectProps from '../../WorkspacesSelection/WorkspacesSelection.d.ts';
 
 export default function Select({
@@ -9,10 +10,15 @@ export default function Select({
   const [selectedOption, setSelectedOption] = useState<null | string>('Notes');
   const [searchValue, setSearchValue] = useState<string>('');
   const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+  const [shouldExpandInput, setShouldExpandInput] = useState(false);
   const dropdownRef = useRef(null);
 
   const isFocus = () => {
     setDropdownIsOpen((wasOpen) => !wasOpen);
+  };
+
+  const toggleAddInput = () => {
+    setShouldExpandInput((wasAddInput) => !wasAddInput);
   };
 
   const onBlur = (event: React.FocusEvent<HTMLInputElement, HTMLElement>) => {
@@ -29,7 +35,7 @@ export default function Select({
   };
 
   return (
-    <div className="select">
+    <div className="select" data-is-dropdown-open={dropdownIsOpen}>
       <span className="select__aria-describe" id="select-describe">
         <span>
           <span id="aria-results"></span>
@@ -37,22 +43,37 @@ export default function Select({
         </span>
       </span>
 
-      <div className="select__wrapper">
-        <p className="select__selected-option">
-          {!searchValue && selectedOption}
-        </p>
-        <div className="select__dropdown" data-value={searchValue}>
-          <input
-            onChange={(event) => {
-              setSearchValue(event.target.value);
-              setSelectedOption(null);
-            }}
-            onFocus={isFocus}
-            onBlur={onBlur}
-            className="select__input"
-            value={searchValue}
-          />
+      <div className="select__view-add">
+        <div className="select__wrapper">
+          <p className="select__selected-option">
+            {!searchValue && selectedOption}
+          </p>
+          <div className="select__dropdown" data-value={searchValue}>
+            <input
+              onChange={(event) => {
+                setSearchValue(event.target.value);
+                setSelectedOption(null);
+              }}
+              onFocus={isFocus}
+              onBlur={onBlur}
+              className="select__search"
+              value={searchValue}
+            />
+          </div>
         </div>
+        <AddItem
+          shouldAnimate={shouldExpandInput}
+          toggle={toggleAddInput}
+          className="notes-button__btn--workspaces btn"
+        />
+        <input
+          className={
+            shouldExpandInput
+              ? 'select__search--add'
+              : 'select__search--add hidden'
+          }
+          type="text"
+        />
       </div>
       {dropdownIsOpen && (
         <div className="select__options">
